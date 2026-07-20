35e13aa72c26fdee69a3448229121fbaf599d722
unknown <osmanomar29@yahoo.com>
2026-07-20 11:24:30 +0200

    Add changes

--- Services/AanmeldenService.cs
+++ Services/AanmeldenService.cs
@@ -1,5 +1,6 @@
 ﻿using BlazorApp1.Data;
 using BlazorApp1.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorApp1.Services
 {
@@ -11,12 +12,32 @@ namespace BlazorApp1.Services
         {
             _context = context;
         }
-
+        public async Task<List<AanmeldenModel>> GetAllAanmeldingenAsync()
+        {
+            return await _context.Aanmeldingen
+                .OrderByDescending(a => a.AanmeldDatum)
+                .ToListAsync();
+        }
         public async Task SaveAanmeldingAsync(AanmeldenModel model)
         {
             model.AanmeldDatum = DateTime.Now;
             await _context.Aanmeldingen.AddAsync(model);
             await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAanmeldingAsync(AanmeldenModel model)
+        {
+            _context.Aanmeldingen.Update(model);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteAanmeldingAsync(int id)
+        {
+            var item = await _context.Aanmeldingen.FindAsync(id);
+            if (item == null) return false;
+
+            _context.Aanmeldingen.Remove(item);
+            await _context.SaveChangesAsync();
+            return true;
         }
     }
 }
